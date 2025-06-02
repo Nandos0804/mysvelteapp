904eb83dd5230fb7c2130dd6b4b3f08e9dfde2f4
Giuseppe Ernandez <giuseppeernandez@hotmail.it>
2025-06-02 19:12:41 +0200

    presentazione

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,17 +8,19 @@
         "This is a paragraph with <em>some</em> infotext.";
     let count: number = $state(0);
 
-
     function increment() {
         count += 1;
     }
-
 </script>
 
 <div class="container mx-auto py-4 mt-30">
-    <h1>hello {name.toUpperCase()}</h1>
-
     <img {src} alt="Logo" />
+
+    <h1 class="font-bold text-red-400 text-2xl py-7">
+        Svelte + tailwindcss
+    </h1>
+
+    <h2>hello {name.toUpperCase()}</h2>
 
     <p class="text-info">This is a paragraph with some infotext.</p>
 
@@ -26,8 +28,10 @@
 
     <p>{@html textFormatted}</p>
 
-
-    <button onclick={increment} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button
+        onclick={increment}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
         Count {count}
         {count === 0 ? "Click me!" : "Clicked!"}
     </button>
@@ -38,8 +42,8 @@
 <style lang="postcss">
     @reference "tailwindcss";
     .text-info {
-        color: red;
-        font-size: 2rem;
+        color: black;
+        font-size: 1.3rem;
         font-weight: bold;
     }
 </style>
